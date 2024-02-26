c3b029e6c60ae9af8611b478aeee842b6f64e94e
Mayank thukral <79597046+mayank91091@users.noreply.github.com>
2024-02-25 23:38:45 -0500

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,27 +1,13 @@
-# Generate random resource group name
-/*
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
-
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
-}
-
-resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
-  prefix = "dns"
+  name     = "my_resource_group" // Static name for the resource group
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  name                = "my_kubernetes_cluster" // Static name for the Kubernetes cluster
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  dns_prefix          = "my_dns_prefix" // Static DNS prefix for the Kubernetes cluster
 
   identity {
     type = "SystemAssigned"
@@ -44,4 +30,3 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     load_balancer_sku = "standard"
   }
 }
-*/
